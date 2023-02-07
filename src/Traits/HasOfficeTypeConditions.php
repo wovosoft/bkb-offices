7ae24a59b5c4133df640ce8034504fef885749cc
Narayan Adhikary <wovosoft@gmail.com>
2023-02-07 12:05:02 +0600

    Added Doc for office type checking properties

--- src/Traits/HasOfficeTypeConditions.php
+++ src/Traits/HasOfficeTypeConditions.php
@@ -5,6 +5,15 @@ namespace Wovosoft\BkbOffices\Traits;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Wovosoft\BkbOffices\Enums\OfficeTypes;
 
+/**
+ * @property-read boolean $is_branch
+ * @property-read boolean $is_corporate_branch
+ * @property-read boolean $is_div_office
+ * @property-read boolean $is_dao_office
+ * @property-read boolean $is_rao_office
+ * @property-read boolean $is_crm_rm_office
+ * @property-read boolean $is_head _office
+ */
 trait HasOfficeTypeConditions
 {
     public function isBranch(): Attribute
