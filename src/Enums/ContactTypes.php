a4dbd5ef6565f18cdfe5914e9023a053485c7ff9
Narayan Adhikary <wovosoft@gmail.com>
2023-11-16 14:42:44 +0600

    Update ContactTypes.php

--- src/Enums/ContactTypes.php
+++ src/Enums/ContactTypes.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace Wovosoft\BkbOffices\Enums;
+
+use Wovosoft\LaravelCommon\Traits\HasEnumExtensions;
+
 enum ContactTypes: string
 {
+    use HasEnumExtensions;
+
     case Email = "email";
     case Phone = "phone";
 }
